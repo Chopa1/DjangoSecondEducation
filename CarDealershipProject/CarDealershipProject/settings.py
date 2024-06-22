f8eb8047e5fa10d0bd19b089e7bd566b72a4f821
Danil <maltzevvadim48@gmail.com>
2024-06-22 18:05:45 +0700

    add crispy

--- CarDealershipProject/CarDealershipProject/settings.py
+++ CarDealershipProject/CarDealershipProject/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inventory'
+    'inventory',
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -67,6 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'CarDealershipProject.wsgi.application'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
